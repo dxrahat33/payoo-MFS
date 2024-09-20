6c8b80b4afdec9bd5e4fad488e80599610447bd8
dxrahat33 <dxrahat33@gmail.com>
2024-09-20 20:35:47 +0600

    add money form complete

--- js/home.js
+++ js/home.js
@@ -1,1 +1,23 @@
-console.log('home added');
+document.getElementById('btn-add-money').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    const addMoneyInput = document.getElementById('input-amount').value;
+    const pinInput = document.getElementById('input-pin').value;
+
+    if (pinInput === '1234') {
+
+    
+        const balance = document.getElementById('account-balance').innerText;
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber + addMoneyNumber;
+        console.log(newBalance);
+        
+        document.getElementById('account-balance').innerText = newBalance;
+
+
+    }
+    else {
+        alert('failed, try again !');
+    }
+})
